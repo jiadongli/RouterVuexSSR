9d543fed7678faed1907a3cc326d1e9768e59cd3
jiadongli <1347507274@qq.com>
2018-10-30 23:12:20 +0800

    xxx

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path=require("path");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+module.exports={
+    entry:path.join(__dirname,'src/index.js'),
+    output:{
+        filename:'bundle.js',
+        path:path.join(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                loader:'vue-loader'
+            }
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugin()
+    ],
+    mode:'development'
+}
